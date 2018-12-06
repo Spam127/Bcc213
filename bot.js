7d926590bd76998c3439f2d3cd07642ad7db9dac
Spam127 <45201082+Spam127@users.noreply.github.com>
2018-12-06 03:59:50 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,26 +5,26 @@ client.on('message',async message => {
   if(message.author.bot || message.channel.type === '-bc') return;
   let args = message.content.split(' ');
   if(args[0] === `-bc`) {
-    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('- **??? ?? ???? ????????? ??????? ???????? ??? ?????**');
-    if(!args[1]) return message.channel.send('- **??? ???? ????? ??????? ??? ?????**');
+    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('- **أنت لا تملك الصلاحيات اللازمة لأستخدام هذا الأمر**');
+    if(!args[1]) return message.channel.send('- **يجب عليك كتابة الرسالة بعد الأمر**');
   
     let msgCount = 0;
     let errorCount = 0;
     let successCount = 0;
-    message.channel.send(`**- [ :bookmark: :: ${msgCount} ] ·??? ??????? ???????**\n**- [ :inbox_tray: :: ${successCount} ] ·??? ??????? ????????**\n**- [ :outbox_tray: :: ${errorCount} ]·??? ??????? ????? ??????**`).then(msg => {
+    message.channel.send(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`).then(msg => {
       message.guild.members.forEach(g => {
         g.send(args.slice(1).join(' ')).then(() => {
           successCount++;
           msgCount++;
-          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ·??? ??????? ???????**\n**- [ :inbox_tray: :: ${successCount} ] ·??? ??????? ????????**\n**- [ :outbox_tray: :: ${errorCount} ]·??? ??????? ????? ??????**`);
+          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
         }).catch(e => {
           errorCount++;
           msgCount++;
-          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ·??? ??????? ???????**\n**- [ :inbox_tray: :: ${successCount} ] ·??? ??????? ????????**\n**- [ :outbox_tray: :: ${errorCount} ]·??? ??????? ????? ??????**`);
+          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
         });
       });
     });
   }
 });
 
-client.login("NTIwMjAzMTQyMTQzNjcyMzIx.DuqdtQ.FBssS0oPUZ_bzdm33Bzs-LX6kGo")
+client.login("NTIwMjA2NjE0MzkzOTEzMzk1.DuqgnA.g4gGa3D5bbf4XKNKDrksRzyuGco")
